3b337218bf915cb0d962efa6b8aa42d5d1c39214
Valentine Ssebuyungo <vssebuyungo@gmail.com>
2023-12-16 10:56:09 +0300

    Finished Front End

--- DOM/Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
+++ DOM/Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
@@ -6,3 +6,15 @@ var playerTwoImage = `dice${randomValue2}.png`;
 
 document.getElementsByClassName("img1")[0].src = "./images/" + playerOneImage;
 document.getElementsByClassName("img2")[0].src = "./images/" + playerTwoImage;
+
+//If player 1 wins
+if (randomValue1 > randomValue2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (randomValue2 > randomValue1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+  
